689e223321f1411c3d3291c25bc10caafbffc016
Rita-anne <annegakiza@gmail.com>
2024-02-12 08:51:37 +0200

    upp

--- src/src.js
+++ src/src.js
@@ -1,21 +1,24 @@
 const colors = ['red','green','blue','yellow','orange']
-result = colors.splice(2,2, "puple", "pink")
-console.log(result)
+const res = colors.splice(2,2, "puple", "pink")
+console.log(res)
 
 // Utilize the copyWithin() method to copy the elements from index 1 to index 3 within the colors array to index 0.
 const color = ['red','green','blue','yellow','orange']
-result = colors.copyWithin(0,1,3)
-console.log(result)
+const resu = colors.copyWithin(0,1,3)
+console.log(resu)
 
 const colour = ['red','green','blue','yellow','orange']
 const result = colour.join(",")
 
 // Apply the find() method to find the first element in the colors array that starts with the letter 'b'.
 const colors1 = ['red','green','blue','yellow','orange']
-result = colors.find()
-
+const startB=(a)=>{
+    if(a.startsWith('b')){
+        return a
+    }
+}
+console.log(colors1.find(startB))
 // Finally, transform all elements of the colors array to uppercase using the toUpperCase() method.
 const colors2 = ['red','green','blue','yellow','orange']
-const upper = colors.map(element => {
-    return element.toUpperCase()
-})
+const changed=colors2.toString().toUpperCase()
+console.log(changed)
