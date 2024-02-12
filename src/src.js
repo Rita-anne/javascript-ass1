c54b492a4cb0ab12cb30e064fc738833a8f6d625
Rita-anne <annegakiza@gmail.com>
2024-02-12 09:01:23 +0200

    Update src.js
    yuuu

--- src/src.js
+++ src/src.js
@@ -1,16 +1,17 @@
 const colors = ['red','green','blue','yellow','orange']
 const res = colors.splice(2,2, "puple", "pink")
 console.log(res)
+console.log(colors)
 
 // Utilize the copyWithin() method to copy the elements from index 1 to index 3 within the colors array to index 0.
 const color = ['red','green','blue','yellow','orange']
-const resu = colors.copyWithin(0,1,3)
+const resu = color.copyWithin(0,1,3)
 console.log(resu)
 
 const colour = ['red','green','blue','yellow','orange']
 const result = colour.join(",")
 
-// Apply the find() method to find the first element in the colors array that starts with the letter 'b'.
+// Apply the find() method to find the first element in the colors array that starts with the letter b.
 const colors1 = ['red','green','blue','yellow','orange']
 const startB=(a)=>{
     if(a.startsWith('b')){
